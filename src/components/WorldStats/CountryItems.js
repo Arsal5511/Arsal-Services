8397dc1473d9fe24f5d493f7a17c42d882d1dac2
Arslan Abbas <arslanabbas5511@gmail.com>
2024-01-02 21:08:40 +0500

    minor style changes in world stats

--- src/components/WorldStats/CountryItems.js
+++ src/components/WorldStats/CountryItems.js
@@ -1,52 +1,91 @@
-import React from 'react'
+import React from "react";
 
 const CountryItems = ({ data }) => {
-
     return (
-        <div className='flex flex-col w-[50%]  items-center justify-center mt-[40px] '>
-            {
-                data?.map((country, index) => {
-                    const countryKeys = Object.keys(country.currencies)
-                    const langKeys = Object.keys(country.languages)
+        <div className="flex flex-wrap flex-1 mt-[40px] w-[90%] sm:w-[60%] md:w-[50%] lg:w-[50%] items-center justify-center space-x-2 space-y-2 mb-3 ">
+            {data?.map((country, index) => {
+                const countryKeys = Object.keys(country.currencies);
+                const langKeys = Object.keys(country.languages);
 
-                    return (
-                        <section key={index} className='flex flex-col md:flex-row items-center justify-between backdrop:z m-4 p-8 md:w-[100%] shadow-lg  my-4 rounded-3xl  bg-[#dbeafe] '>
-                            <div id='data' className="border-black-500 justify-start">
-                                <h1 className='font-bold text-3xl justify-center text-center md:text-left p-3 pl-0'>{country.name.common}</h1>
-                                <p >Total Population:   {country.population}</p>
-                                <p >Capital City:   {country.capital}</p>
-                                <p >Area:   {country.area}</p>
-                                <ul>
-                                    {countryKeys?.map((i) => {
-                                        return (
-                                            <div key={i}>
-                                                <span>Currency: ({i})</span>
-                                                <span>{country.currencies[i]?.name}<br></br></span>
-                                                <span>Currency symbol:  {country.currencies[i]?.symbol}</span>
-                                            </div>
-                                        )
-                                    })}
-                                </ul>
-                                <ol>Languages:
-                                    {langKeys?.map((i) => {
-                                        return (
-                                            <div key={i}>
-                                                <li> (   {i}  )   <span>{country.languages[i]}</span>
-                                                </li >
-                                            </div>
-                                        )
-                                    })}
-                                </ol>
+                return (
+                    <div className="flex px-3 bg-blue-800 items-center justify-center  rounded-xl  backdrop:z shadow-lg " >
+                        <section
+                            key={index}
+                            className="flex flex-col rounded-xl max-w-[350px] items-center justify-center  my-4">
+
+                            <div id=" upper_section " className="flex items-center bg-white  p-3 ">
+                                <div
+                                    id="img"
+                                    className=" w-[50%] mr-4  "
+                                >
+                                    <img
+                                        className="backdrop-blur-lg shadow-2xl  "
+                                        src={country.flags.png}
+                                        alt={country.flags.alt}
+                                    />
+                                </div>
+                                <div
+                                    id="country_name"
+                                    className=" ml-4  w-[50%] text-white-800  "
+                                >
+                                    <h2 className="text-center font-bold text-xl">
+                                        {country.name.official}
+                                    </h2>
+                                    <h2 className="text-center font-bold text-lg">
+                                        Capital City
+                                    </h2>
+                                    <h2 className="text-center font-light text-lg">
+                                        {country.capital}
+                                    </h2>
+                                </div>
                             </div>
-                            <div id='img' className=" mt-5 flex justify-end flex-col items-center md:mt-0 ">
-                                <img className='w-[100px] h-[100px] rounded-[50%] md:rounded-none md:w-[150px]  ' src={country.flags.png} alt={country.flags.alt} width={100} />
+
+                            <div id="data" className="bg-[rgb(255,255,255)] p-5 w-[100%] ">
+                                <tbody>
+                                    <tr>
+                                        <td className="py-1 px-2 font-semibold">
+                                            Total Population:
+                                        </td>
+                                        <td className="py-1 px-2">{country.population}</td>
+                                    </tr>
+                                    <tr>
+                                        <td className="py-1 px-2 font-semibold">Area:</td>
+                                        <td className="py-1 px-2">{country.area}</td>
+                                    </tr>
+                                    <tr>
+                                        <td className="py-1 px-2 font-semibold">
+                                            Currency:
+                                        </td>
+                                        {countryKeys?.map((i) => {
+                                            return (
+                                                <td key={i} className='py-1 px-2 ' >
+                                                    {country.currencies[i]?.symbol} ({i})
+                                                </td>
+                                            );
+                                        })}
+                                    </tr>
+                                    <tr>
+                                        <td className="py-1 px-2 font-semibold">
+                                            Languages:
+                                        </td>
+                                        {langKeys?.map((i) => {
+                                            return (
+                                                <td key={i} className='py-1 px-2 ' >
+                                                    ( {i} ) {country.languages[i]}
+                                                </td>
+                                            );
+                                        })}
+                                    </tr>
+                                </tbody>
                             </div>
+
+
                         </section>
-                    )
-                })
-            }
-        </div>
-    )
-}
+                    </div>
+                );
+            })}
+        </div >
+    );
+};
 
-export default CountryItems
+export default CountryItems;
